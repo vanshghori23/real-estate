57d2126fa6aa01903cacce04fbee297430f95614
vanshghori23 <ivansh81024@student.sfit.ac.in>
2024-10-12 14:54:56 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+        console.log('Connected to db');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+
 
 const app = express();
 
@@ -6,4 +20,3 @@ app.listen(3000, () => {
     console.log('Server is running on port 3000!!');
   }
 );
-  
