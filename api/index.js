fe3a13bab0bfd09b5f91f587dbd921ec23f6ddd2
vanshghori23 <ivansh81024@student.sfit.ac.in>
2024-10-16 16:42:31 +0530

    create user update api

--- api/index.js
+++ api/index.js
@@ -3,12 +3,13 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 mongoose
     .connect(process.env.MONGO)
     .then(() => {
-        console.log('Connected to db');
+        console.log('Connected to MongoDB');
     })
     .catch((err) => {
         console.log(err);
@@ -19,6 +20,8 @@ mongoose
 const app = express();
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000!!');
