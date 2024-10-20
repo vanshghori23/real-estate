c2071d65243112a346e6ed28e26ec213a1179432
vanshghori23 <ivansh81024@student.sfit.ac.in>
2024-10-20 13:16:15 +0530

    fix app.jsx file for signin

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Signin from "./pages/Signin";
+import Signin from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
@@ -17,7 +17,7 @@ export default function App() {
   <Header />
   <Routes>
     <Route path="/" element={<Home />} />
-    <Route path="/sign-in" element={<Signin />} />
+    <Route path="/sign-in" element={<SignIn />} />
     <Route path="/sign-up" element={<SignUp />} />
     <Route path="/about" element={<About />} />
     <Route path="/search" element={<Search />} />
